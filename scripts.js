68d17ab64609da18fa24fb0d4b74e6202ac87ef4
aoforfie <aoforfie@gmail.com>
2017-07-11 12:50:20 -0400

    Updated scripts.js by Afia
    Afia's answers...

--- scripts.js
+++ scripts.js
@@ -1,32 +1,114 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
+//http://www.jstips.co/en/javascript/calculate-the-max-min-value-from-an-array/
+  largestEl: function(array){  
+    // your code here    
+    return Math.max.apply(null,array);
   },
-  
-  reversed: function(){  
+  // http://eddmann.com/posts/ten-ways-to-reverse-a-string-in-javascript/
+  reversed : function reverse (string) { 
     // your code here
+    return string.split('').reverse().join('');
   },
-
-  loudSnakeCase: function(){  
+//https://en.wikipedia.org/wiki/Snake_case
+//http://epeli.github.io/underscore.string/
+  loudSnakeCase: function(sentence){  
     // your code here
+    return camelize(sentence).titleize().underscored();
   },
-
-  compareArrays: function(){ 
+ //https://stackoverflow.com/questions/4025893/how-to-check-identical-array-in-most-efficient-way
+  compareArrays: function arraysEqual(array1,array2){ 
     // your code here (replace the return)
-    return "Finish compareArrays first!" 
+    if(array1.length !== array2.length)
+
+        return false;
+
+      for(var i = array1.length; i--;) {
+
+        if(array1[i] !== array2[i])
+
+            return false;    }
+
+    
+    return true; 
   },
 
-  fizzBuzz: function(){  
+  fizzBuzz: function(digit){  
     // your code here
+  
+
+        var digit = prompt ("Please input a number:");
+
+        for ( var digit = 1; digit < digit.length; digit ++ ) {
+
+          if ( digit % 15 === 0) {
+
+            return "FIZZBUZZ";
+
+        }
+
+          
+          else if (digit % 3 === 0) {
+
+            return "FIZZ";
+
+          }
+
+          
+
+          else if (digit % 5 === 0) {
+
+            return "BUZZ";
+
+          }
+
+          
+
+          else {
+
+            return digit;
+
+          }
+
+        }
+
+      }
+
+      
+
+      // https://stackoverflow.com/questions/9914216/how-do-i-separate-an-integer-into-separate-digits-in-an-array-in-javascript
+
+      // const n = 123456;
+
+      Array.from(digit.toString()).map(Number);
+
   },
 
-  myMap: function(){  
+  myMap: function(els,fxn){  
     // your code here
-  },
+        var els =array.from(array.toString()).map(Number);
 
-  primes: function(){  
+        var fxn = function {
+
+          return ;
+
+        };
+
+    },
+
+  primes: function(PDigit){  
     // your code here
+     //https://stackoverflow.com/questions/30150251/find-all-prime-numbers-from-array
+
+         for (int j=2; j<array[i]; j++){
+
+    if(array[PDigit]%j==0){
+
+            isPrime = false;
+
+            break;
+
+        }
   },
 }
